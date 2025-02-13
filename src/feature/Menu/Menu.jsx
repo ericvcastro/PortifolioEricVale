668eb4b4f2a7343c58558ac4e612e781c868d31a
Eric do Vale de Castro <eric.valecastro@unilever.com>
2025-02-12 23:24:28 -0300

    feat: Create states to validation on the menu bar

---
+++ src/feature/Menu/Menu.jsx
@@ -0,0 +1,45 @@
+import React, { useState } from "react";
+
+
+const Menu = () => {
+    const [controllMenu, setControllMenu] = useState(false)
+
+    const openMenu = () => {
+        setControllMenu(!controllMenu)
+    }
+
+    return (
+        <div onClick={openMenu} style={{backgroundColor: "black"}}>
+            {controllMenu ? (
+                <h1 backgroundColor="red">TRUE</h1>
+            ) : (
+                <svg width="100" height="100" xmlns="http://www.w3.org/2000/svg">
+                    <rect
+                        x="10" y="10"
+                        width="15" height="15"
+                        fill="black"
+                        stroke="white"
+                        stroke-width="3"
+                    />
+                    <rect
+                        x="10" y="40"
+                        width="15" height="15"
+                        fill="black"
+                        stroke="white"
+                        stroke-width="3"
+                    />
+                    <rect
+                        x="10" y="70"
+                        width="15" height="15"
+                        fill="black"
+                        stroke="white"
+                        stroke-width="3"
+                    />
+                    </svg>
+            )}
+        </div>
+    )
+
+}
+
+export default Menu;
